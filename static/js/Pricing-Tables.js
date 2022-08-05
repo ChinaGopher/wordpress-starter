693968591777c196cf063f7b0fc871d3e254a8a8
ChinaGopher <105952775+ChinaGopher@users.noreply.github.com>
2022-08-05 20:20:33 +0000

    edited documentation

--- static/js/Pricing-Tables.js
+++ static/js/Pricing-Tables.js
@@ -1,4 +1,6 @@
 (function() {
   var MutationObserver, Util, WeakMap, getComputedStyle, getComputedStyleRX,
     bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
-    indexOf = [].indexOf || function(item) { 
+    indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+
+  Uti
