34fb6706eaf366e3040754383a426572a89fa4cc
ChinaGopher <105952775+ChinaGopher@users.noreply.github.com>
2022-08-06 12:33:49 +0000

    updating styling

--- static/js/Pricing-Tables.js
+++ static/js/Pricing-Tables.js
@@ -3,4 +3,8 @@
     bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
-  Uti
+  Util = (function() {
+    function Util() {}
+
+    Util.prototype.extend = function(custom, defaults) {
+ 
