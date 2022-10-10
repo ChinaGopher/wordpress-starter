07ac3ec4babbe463d43dc9ad929c2f7bd348bba5
ChinaGopher <105952775+ChinaGopher@users.noreply.github.com>
2022-10-10 12:22:05 +0000

    add codacy config file

--- static/js/Pricing-Tables.js
+++ static/js/Pricing-Tables.js
@@ -213,4 +213,8 @@
         var j, len, ref, results;
         ref = this.boxes;
         results = [];
-        for (j = 0, len = ref.length; j 
+        for (j = 0, len = ref.length; j < len; j++) {
+          box = ref[j];
+          results.push(box);
+        }
+        return resul
